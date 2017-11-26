e3033950d2f330a18f93d0fc1eea3d147ebd13d6
cshepherd2709 <cshepherd2709@gmail.com>
2017-11-26 11:54:00 -0500

    Recomendacion de Tiendas v2

--- hackathon/8-pantalla-error-general.php
+++ hackathon/8-pantalla-error-general.php
@@ -98,15 +98,18 @@ if (ValidarFechaReclamos($objReclamo) == "No") {
                             <?php
                             $str = explode(",", ObtenerCoordenadasGPS("Carlos Villaran 140 La Victoria Lima"));
                             $listTiendas = ObtenerTiendas($str[0], $str[1]);
-                            $i = 1;
+                            $i = 0;
 
                             foreach ($listTiendas as $objTienda) {
                                 $i++;
-                                echo $objTienda->zona . "<br/>";
-                                echo $objTienda->direccion . " " . $objTienda->distrito . "<br/>";
-                                echo $objTienda->horario . "<br/><br/>";
-                                if ($i > 2) {
-                                    return "listo";
+                                if ($i == 1) {
+                                } else {
+                                    echo $objTienda->zona . "<br/>";
+                                    echo $objTienda->direccion . " " . $objTienda->distrito . "<br/>";
+                                    echo $objTienda->horario . "<br/><br/>";
+                                    if ($i > 2) {
+                                        return "listo";
+                                    }
                                 }
                             }
                             ?>
